1dbdfd43efca799bcefbec740d67f4cbef0a1c98
oliviajoyyy <127996216+oliviajoyyy@users.noreply.github.com>
2023-04-29 00:52:32 -0700

    Add musicLessons.sql
    Updated SQL code after physical design, based on Roy's initial code.

---
+++ musicLessons.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE musicLessons;
+
+USE musicLessons;
+
+CREATE TABLE Instructor (
+instructorID INTEGER PRIMARY KEY UNIQUE NOT NULL,
+instructorName VARCHAR(50) NOT NULL,
+email VARCHAR(50) UNIQUE NOT NULL,
+phoneNumber VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE Customer (
+customerID INTEGER PRIMARY KEY UNIQUE NOT NULL,
+customerName VARCHAR(50) NOT NULL,
+email VARCHAR(50) UNIQUE NOT NULL,
+phoneNumber VARCHAR(15) UNIQUE NOT NULL
+);
+
+CREATE TABLE Location (
+LocationID INTEGER PRIMARY KEY UNIQUE NOT NULL,
+streetAddress VARCHAR(50) NOT NULL,
+city VARCHAR(30) NOT NULL,
+state VARCHAR(4) NOT NULL,
+zip VARCHAR(5) NOT NULL,
+description TEXT NOT NULL,
+maxParticipants INTEGER NOT NULL
+);
+
+CREATE TABLE Lesson (
+lessonID INTEGER PRIMARY KEY UNIQUE NOT NULL,
+lessonName VARCHAR(25) NOT NULL,
+instrument VARCHAR(15) NOT NULL,
+startTime TIME NOT NULL,
+endTime TIME NOT NULL,
+cost DECIMAL(5,2) NOT NULL,
+description TEXT NOT NULL,
+locationID INTEGER NOT NULL,
+FOREIGN KEY (locationID) REFERENCES Location(locationID)
+);
+
+CREATE TABLE CustomerLesson (
+customerID INTEGER UNIQUE NOT NULL,
+lessonID INTEGER UNIQUE NOT NULL,
+PRIMARY KEY (customerID, lessonID),
+FOREIGN KEY (customerID) REFERENCES Customer(customerID),
+FOREIGN KEY (lessonID) REFERENCES Lesson(lessonID)
+);
+
+CREATE TABLE InstructorLesson (
+instructorID INTEGER UNIQUE NOT NULL,
+lessonID INTEGER UNIQUE NOT NULL,
+PRIMARY KEY (instructorID, lessonID),
+FOREIGN KEY (instructorID) REFERENCES Instructor(instructorID),
+FOREIGN KEY (lessonID) REFERENCES Lesson(lessonID)
+);
+
+CREATE TABLE InstructorSpecialty (
+instructorID INTEGER UNIQUE NOT NULL,
+specialty VARCHAR(15) NOT NULL,
+PRIMARY KEY (instructorID, specialty),
+FOREIGN KEY (instructorID) REFERENCES Instructor(instructorID)
+);
+
+CREATE TABLE LessonDay (
+lessonID INTEGER UNIQUE NOT NULL,
+day CHAR(3) NOT NULL,
+PRIMARY KEY (lessonID, day),
+FOREIGN KEY (lessonID) REFERENCES lesson(lessonID)
+);
