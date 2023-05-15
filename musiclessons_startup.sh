bde4ed968ae7d2469d7f928a86bb2c9c2b29fd9f
Ibarra-M <33313125+Ibarra-M@users.noreply.github.com>
2023-05-15 10:37:26 -0700

    startup script
    Startup script to run Apache and MySQL.

---
+++ musiclessons_startup.sh
@@ -0,0 +1,10 @@
+//For linux. Have apache and mysql installed.
+//Place me in the following directory: "/etc/intit.d"
+
+//Starts Apache web server
+sudo service apache2 start
+
+//Starts service mysql start
+sudo service mysql start
+
+
